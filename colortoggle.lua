4d723c84be0a0b8d3de7574df6cfb5a03b3ab271
Cody Creech <codycreech@me.com>
2018-11-24 10:11:21 -0500

    moved colortoggles to its own file

---
+++ colortoggle.lua
@@ -0,0 +1,23 @@
+colorToggleR = {
+  x = toggleBlockR.x,
+  y = toggleBlockR.y,
+  w = 10,
+  h = 20,
+  dragging = {active = false, diffX = 0}
+}
+
+colorToggleG = {
+  x = toggleBlockG.x,
+  y = toggleBlockG.y,
+  w = 10,
+  h = 20,
+  dragging = {active = false, diffX = 0}
+}
+
+colorToggleB = {
+  x = toggleBlockB.x,
+  y = toggleBlockB.y,
+  w = 10,
+  h = 20,
+  dragging = {active = false, diffX = 0}
+}
