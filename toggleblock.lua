b404b17a5de7f1bcbca1e61ef526b01069981f5b
Cody Creech <codycreech@me.com>
2018-11-24 10:11:05 -0500

    moved toggleblocks to its own file

---
+++ toggleblock.lua
@@ -0,0 +1,20 @@
+toggleBlockR = {
+  x = 150,
+  y = 200,
+  w = 120,
+  h = 20
+}
+
+toggleBlockG = {
+  x = 150,
+  y = 225,
+  w = 120,
+  h = 20
+}
+
+toggleBlockB = {
+  x = 150,
+  y = 250,
+  w = 120,
+  h = 20
+}
